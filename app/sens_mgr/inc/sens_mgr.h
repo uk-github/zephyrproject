fbdf95e3d90d046af7bf54f8a87403c1540231da
uk <uttam.cuet@gmail.com>
2025-08-10 19:43:48 +0600

    sens manager updated

--- app/sens_mgr/inc/sens_mgr.h
+++ app/sens_mgr/inc/sens_mgr.h
@@ -1,5 +1,12 @@
 #pragma once
 #include "loggers.h"
 
+typedef enum {
+    SENS_ACC, 
+    SENS_GYRO,
+    SENS_IR,
+    SENS_MAX
+} sens_type_t;
+
 int sens_mgr_init(void);
 int sens_mgr_start(void);
